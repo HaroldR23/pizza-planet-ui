98d0a6728586c126aaaf00b7fec8bee70dd61ee6
Harold Rodriguez <107003395+HaroldR23@users.noreply.github.com>
2022-10-17 11:27:19 -0300

    feat: PIZ5 Fixing validation report endpoint (#3)

--- js/report/get-report.js
+++ js/report/get-report.js
@@ -1,19 +1,29 @@
 fetch('http://127.0.0.1:5000/report/')
     .then(response => response.json())
     .then(report => {
-        let map_customers = {}
-        report.best_costumers.forEach((customer, i) => {
-            map_customers[`name${i + 1}`] = customer.client_name
-        });
-        report.best_customers = map_customers
-        let date = new Date(report.month_more_revenue.month);
-        report.month_more_revenue.month = date.toLocaleString("en-US", { month: "long"});
-        let template = createReportTemplate(report);
-        $("#report").append(template);
+        if(report.length === 0) {
+            let reportEmptyTemplate = createReportEmptyTemplate({message:"Don't froget populate the database"});
+            $("#report").append(reportEmptyTemplate)
+        } else {
+            let map_customers = {}
+            report.best_costumers.forEach((customer, i) => {
+                map_customers[`name${i + 1}`] = customer.client_name
+            });
+            report.best_customers = map_customers
+            let date = new Date(report.month_more_revenue.month);
+            report.month_more_revenue.month = date.toLocaleString("en-US", { month: "long"});
+            let template = createReportTemplate(report);
+            $("#report").append(template);
+        }
     });
 
 
 function createReportTemplate(report) {
     let template = $("#report-template")[0].innerHTML;
     return Mustache.render(template, report);
+}
+
+function createReportEmptyTemplate(message) {
+    let template = $("#report-empty")[0].innerHTML;
+    return Mustache.render(template, message);
 }
